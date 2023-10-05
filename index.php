5793e417330f82b264b1d20a90c676fb92b66bdf
Alexsander <131073044+Alexsander-ETEC@users.noreply.github.com>
2023-10-04 22:26:45 -0300

    Atualizar o index.php

--- index.php
+++ index.php
@@ -4,21 +4,44 @@
 <head>
 	<link rel='stylesheet' href='.\index.css'>
 	<title>calculadora</title>
+ <?php include './módulos/bootstrap' ?>
 </head>
 
 <body>
+ <?php
+  $x = $_GET['x'];
+  $y = $_GET['y'];
+  $o = $_GET['op'];
+
+  function calc($x, $y, $operacao) {
+   switch ($operacao) {
+    case 1:
+     echo $x + $y;
+     break;
+    case 2:
+     echo $x - $y;
+     break;
+    case 3:
+     echo $x * $y;
+     break;
+    case 4:
+     if ($y == 0) {
+      echo 'Infinity';
+     } else {
+      echo $x / $y;
+     }
+     break;
+    }
+  }
+ ?>
 	<h1>alexsander</h1>
-	<form id='formulario' method='get' action='.'> <!-- oninput='alert(operacao.value);switch(operacao.value){case 1:res.value=parseFloat(numero_x.value)+parseFloat(numero_y.value);break;case 2:res.value=parseFloat(numero_x.value)-parseFloat(numero_y.value);break;case 3:res.value=parseFloat(numero_x.value)*parseFloat(numero_y.value);break;case 4:res.value=parseFloat(numero_x.value)/parseFloat(numero_y.value);break;}' -->
-		<div id='caixa-maior'>
-			<div id='entrada-x'>
-				<input id='numero_x' name='x' type='number' value='<?php if (isset($_GET['x'])) {
-																		echo (float)$_GET['x'];
-																	} ?>'>
+	<form id='formulario' method='get' action='.'>
+		<div id='caixa'>
+			<div id='entrada_x'>
+				<input id='numero_x' name='x' type='number' value='<?php echo $x ?>'>
 			</div>
 			<div>
-				<select name='op' id='operacao' value='<?php if (isset($_GET['op'])) {
-															echo (float)$_GET['op'];
-														} ?>'>
+				<select id='operacao' name='op' value='<?php echo $o ?>'>
 					<option value='1'>soma</option>
 					<option value='2'>subtração</option>
 					<option value='3'>multiplicação</option>
@@ -26,34 +49,14 @@
 				</select>
 			</div>
 			<div id='entrada-y'>
-				<input id='numero_y' name='y' type='number' value='<?php if (isset($_GET['y'])) {
-																		echo (float)$_GET['y'];
-																	} ?>'>
+				<input id='numero_y' name='y' type='number' value='<?php echo $y ?>'>
 			</div>
 			<div id='saida'>
 				<span>resultado -> <?php if (isset($_GET['op']) and isset($_GET['x']) and isset($_GET['y'])) {
 										$x = (float)$_GET['x'];
 										$y = (float)$_GET['y'];
 										$o = (float)$_GET['op'];
-										switch ($o) {
-											case 1:
-												echo $x + $y;
-												break;
-											case 2:
-												echo $x - $y;
-												break;
-											case 3:
-												echo $x * $y;
-												break;
-											case 4:
-												if ($y == 0) {
-													echo 'Infinity';
-												}
-												echo $x / $y;
-												break;
-											default:;
-										}
-									} ?></span> <!-- <output name='res' for='numero_x numero_y'></output> -->
+										 ?></span> <!-- <output name='res' for='numero_x numero_y'></output> -->
 			</div>
 		</div>
 		<div id='botoes'>
