a75c5fa07246f700de2075117771aba56778d2a1
Alexsander <alexsander.santana2@etec.sp.gov.br>
2023-10-11 20:38:02 -0300

    modulo bootstrap e lib

--- index.php
+++ index.php
@@ -4,64 +4,39 @@
 <head>
 	<link rel='stylesheet' href='.\index.css'>
 	<title>calculadora</title>
-	<?php include './modulos/bootstrap' ?>
+	<?php include '.\modulos\bootstrap.html' ?>
 </head>
 
 <body>
 	<?php
-	$x = isset($_GET['x']);
-	$y = isset($_GET['y']);
-	$o = isset($_GET['op']);
-
-	function calc($x, $y, $operacao)
-	{
-		switch ($operacao) {
-			case 1:
-				echo $x + $y;
-				break;
-			case 2:
-				echo $x - $y;
-				break;
-			case 3:
-				echo $x * $y;
-				break;
-			case 4:
-				if ($y == 0) {
-					echo 'Infinity';
-				} else {
-					echo $x / $y;
-				}
-				break;
-		}
-	}
+	include '.\modulos\lib.php';
+	$x = isset($_GET['x']) ? $_GET['x'].'' : '';
+	$y = isset($_GET['y']) ? $_GET['y'].'' : '';
+	$o = isset($_GET['op']) ? $_GET['op'].'' : '';
 	?>
 	<h1>alexsander</h1>
-	<form id='formulario' method='get' action='.'>
+	<form id='formulario' method='get'>
 		<div id='caixa'>
 			<div id='entrada_x'>
-				<input id='numero_x' name='x' type='number' value='<?php echo $x ?>'>
+				<input id='numero_x' name='x' type='number' step='0.001' value=<?php echo $x ?>>
 			</div>
 			<div>
-				<select id='operacao' name='op' value='<?php echo $o ?>'>
-					<option value='1'>soma</option>
+				<select id='operacao' class='form-select' name='op'>
+					<option <?php echo $o ?> value='1'>soma</option>
 					<option value='2'>subtração</option>
 					<option value='3'>multiplicação</option>
 					<option value='4'>divisão</option>
 				</select>
 			</div>
 			<div id='entrada-y'>
-				<input id='numero_y' name='y' type='number' value='<?php echo $y ?>'>
+				<input id='numero_y' name='y' type='number' step='0.001' value='<?php echo $y ?>'>
 			</div>
 			<div id='saida'>
-				<span>resultado -> <?php if (isset($_GET['op']) and isset($_GET['x']) and isset($_GET['y'])) {
-										$x = (float)$_GET['x'];
-										$y = (float)$_GET['y'];
-										$o = (float)$_GET['op'];
-									} ?></span> <!-- <output name='res' for='numero_x numero_y'></output> -->
+				<span>resultado -> <?php echo calcular($x, $y, $o); ?></span> <!-- <output name='res' for='numero_x numero_y'></output> -->
 			</div>
 		</div>
 		<div id='botoes'>
-			<input type='submit' class='btn'>
+			<input type='submit' class='btn btn-primary'>
 		</div>
 	</form>
 </body>
