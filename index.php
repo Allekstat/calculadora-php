599fc86bef5f893b50aa48127d10272c852ee7e8
Alexsander <131073044+Alexsander-ETEC@users.noreply.github.com>
2023-10-24 20:38:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,43 +2,38 @@
 <html>
 
 <head>
-	<link rel='stylesheet' href='.\index.css'>
-	<title>calculadora</title>
-	<?php include '.\modulos\bootstrap.html' ?>
+
+<link rel='stylesheet' href='.\index.css'>
+<title>calculadora</title>
+<?php include '.\includes\bootstrap.html' ?>
+
 </head>
 
 <body>
-	<?php
-	include '.\modulos\lib.php';
-	$x = isset($_GET['x']) ? $_GET['x'].'' : '';
-	$y = isset($_GET['y']) ? $_GET['y'].'' : '';
-	$o = isset($_GET['op']) ? $_GET['op'].'' : '';
-	?>
-	<h1>alexsander</h1>
-	<form id='formulario' method='get'>
-		<div id='caixa'>
-			<div id='entrada_x'>
-				<input id='numero_x' name='x' type='number' step='0.001' value=<?php echo $x ?>>
-			</div>
-			<div>
-				<select id='operacao' class='form-select' name='op'>
-					<option <?php echo $o ?> value='1'>soma</option>
-					<option value='2'>subtração</option>
-					<option value='3'>multiplicação</option>
-					<option value='4'>divisão</option>
-				</select>
-			</div>
-			<div id='entrada-y'>
-				<input id='numero_y' name='y' type='number' step='0.001' value='<?php echo $y ?>'>
-			</div>
-			<div id='saida'>
-				<span>resultado -> <?php echo calcular($x, $y, $o); ?></span> <!-- <output name='res' for='numero_x numero_y'></output> -->
-			</div>
-		</div>
-		<div id='botoes'>
-			<input type='submit' class='btn btn-primary'>
-		</div>
-	</form>
+
+<?php
+include '.\includes\funcoes.php';
+$x = isset($_GET['x']) ? $_GET['x'] : '';
+$y = isset($_GET['y']) ? $_GET['y'] : '';
+$o = isset($_GET['op']) ? $_GET['op'] : '';
+?>
+
+<h1>alexsander</h1>
+
+<form method='get'>
+	<label for='numero_x'>Número A</label>
+	<input id='numero_x' name='x' type='number' step='0.001' <?php echo 'value="'.$x.'"'; ?>>
+	<select id='operacao' name='op'>
+		<option <?php if ($o == 1) { echo 'selected'; } ?> value='1'>soma</option>
+		<option <?php if ($o == 2) { echo 'selected'; } ?> value='2'>subtração</option>
+		<option <?php if ($o == 3) { echo 'selected'; } ?> value='3'>multiplicação</option>
+		<option <?php if ($o == 4) { echo 'selected'; } ?> value='4'>divisão</option>
+	</select>
+	<input id='numero_y' name='y' type='number' step='0.001' <?php echo 'value="'.$y.'"'; ?>>
+	<span>resultado -> <?php echo calcular($x, $y, $o); ?></span>
+	<input type='submit'>
+</form>
+
 </body>
 
 </html>
